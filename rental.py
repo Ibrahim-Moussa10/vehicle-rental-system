e281eec6d105a9809a8c9fcfef3f0de5e7618475
Ibrahim-Moussa10 <i.moussa.14.im@gmail.com>
2025-02-27 21:51:07 +0200

    overriding display info for inginr capacity and making rental price as a private attribute + providing setter and getter method for rental price pd

--- rental.py
+++ rental.py
@@ -1,17 +1,30 @@
 class Vehicle:
-    def __init__(self,brand, module, year, rental_price_per_day,seating_capacity):
+    def __init__(self,brand, module, year, rental_price_per_day,seating_capacity, engine_capacity):
         self.brand = brand
         self.module = module
         self.year = year
-        self.rental_price_per_day = rental_price_per_day
+        self.__rental_price_per_day = rental_price_per_day
         self.seating_capacity = seating_capacity
+        self.engine_capacity = engine_capacity
     
     def display_info(self):
         print(f"Brand: {self.brand}")
         print(f"Module: {self.module}")
         print(f"Year: {self.year}")
-        print(f"Rental price per day: {self.rental_price_per_day}")
+        print(f"Rental price per day: {self.__rental_price_per_day}")
         print(f"Seating capacity: {self.seating_capacity}")
+        print(f"Engine capacity: {self.engine_capacity}")
 
     def rental_cost(self, days):
         return self.rental_price_per_day * days
+    
+    def get_rental_price(self):
+        return self.__rental_price_per_day
+    
+    def get_rental_price(self)
+        return self.__rental_price_per_day
+    def set_rental_price(self, price):
+        if price > 0:
+            self.__rental_price_per_day = price
+        else:
+            print("Invalid price")
