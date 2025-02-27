6ee5df9af0f61b1785db7db6e6c57ce7371dfe9d
Ibrahim-Moussa10 <i.moussa.14.im@gmail.com>
2025-02-27 21:42:17 +0200

    defining rental cost and adding seat capacity attribute

--- rental.py
+++ rental.py
@@ -1,12 +1,17 @@
 class Vehicle:
-    def __init__(self,brand, module, year, rental_price_per_day):
+    def __init__(self,brand, module, year, rental_price_per_day,seating_capacity):
         self.brand = brand
         self.module = module
         self.year = year
         self.rental_price_per_day = rental_price_per_day
+        self.seating_capacity = seating_capacity
     
     def display_info(self):
         print(f"Brand: {self.brand}")
         print(f"Module: {self.module}")
         print(f"Year: {self.year}")
         print(f"Rental price per day: {self.rental_price_per_day}")
+        print(f"Seating capacity: {self.seating_capacity}")
+
+    def rental_cost(self, days):
+        return self.rental_price_per_day * days
